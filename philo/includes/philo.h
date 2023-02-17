0ce420d2390f59481649dbd66cba166d6c11944a
etachott <etachott@student.42sp.or.br>
2023-02-17 19:15:17 -0300

    feat(validation): validate_argv is now stronger!

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,13 +6,28 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 23:02:34 by edu               #+#    #+#             */
-/*   Updated: 2023/02/16 20:23:43 by etachott         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:05:06 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # include <stdio.h>
 # include <pthread.h>
+# include <stdlib.h>
 
-int	validate_argv(int argc, char *argv[]);
+typedef struct s_args {
+	int	p_quantity;
+	int	p_die;
+	int	p_eat;
+	int	p_sleep;
+	int	p_eat_time;
+}				t_args;
+
+t_args		*init_args(char *argv[]);
+int			validate_argv(int argc, char *argv[]);
+
+/* Utils functions */
+long int	ft_atoi(const char *str);
+void		*ft_calloc(size_t count, size_t size);
+int			ft_isdigit(int c);
 #endif
--- philo/sources/validate_argv.c
+++ philo/sources/validate_argv.c
@@ -6,16 +6,59 @@
 /*   By: etachott <etachott@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 20:11:30 by etachott          #+#    #+#             */
-/*   Updated: 2023/02/16 20:11:47 by etachott         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:12:15 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+static int	check_extremes(char *argv[])
+{
+	int	max;
+	int	min;
+	int	index;
+
+	max = 2147483647;
+	min = -2147483648;
+	index = 0;
+	while (argv[index])
+	{
+		if (ft_atoi(argv[index]) > max || ft_atoi(argv[index]) < min)
+			return (0);
+		index++;
+	}
+	return (1);
+}
+
+static int	check_for_words(char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	validate_argv(int argc, char *argv[])
 {
 	(void) argv;
-	if (argc < 4 || argc > 5)
+	if (argc < 5 || argc > 6)
+		return (0);
+	if (!check_for_words(argv))
+		return (0);
+	if (!check_extremes(argv))
 		return (0);
 	return (1);
 }
