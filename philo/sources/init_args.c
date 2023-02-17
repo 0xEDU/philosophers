00b0feabbb5a3a56ffb3c1834dd7968feb58ad5d
etachott <etachott@student.42sp.or.br>
2023-02-17 19:15:48 -0300

    feat(init_args): added args struct and its initialization

---
+++ philo/sources/init_args.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_args.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 18:13:46 by edu               #+#    #+#             */
+/*   Updated: 2023/02/17 18:42:04 by edu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+t_args	*init_args(char *argv[])
+{
+	t_args	*new;
+
+	new = ft_calloc(sizeof(t_args), 1);
+	new->p_quantity = ft_atoi(argv[1]);
+	new->p_die = ft_atoi(argv[2]);
+	new->p_eat = ft_atoi(argv[3]);
+	new->p_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		new->p_eat_time = ft_atoi(argv[5]);
+	return (new);
+}
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,34 +6,46 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 23:02:09 by edu               #+#    #+#             */
-/*   Updated: 2023/02/16 20:36:15 by etachott         ###   ########.fr       */
+/*   Updated: 2023/02/17 18:44:00 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+void	eat(void)
+{
+	printf("Philosopher is eating.\n");
+}
+
+void	sleep(void)
+{
+	printf("Philosopher is sleeping.\n");
+}
+
+void	think(void)
+{
+	printf("Philosopher is thinking.\n");
+}
+
 void	*simulation(void *arg)
 {
-	long *id;
-
-	id = (long *) arg;
-	printf("Thread: %ld\n", *id);
-	printf("New thread ;D\n");
+	(void)arg;
+	eat();
+	sleep();
+	think();
 	return (NULL);
 }
 
 int	main(int argc, char *argv[])
 {
 	pthread_t	thread;
-	int			index;
+	t_args		*args;
 
 	if (!validate_argv(argc, argv))
 		return (1);
-	index = 0;
-	while (index++ < 3)
-	{
-		pthread_create(&thread, NULL, simulation, (void *)&thread);
-	}
-	printf("Exited!\n");
+	args = init_args(argv);
+	printf("%p\n", &args);
+	pthread_create(&thread, NULL, simulation, NULL);
+	pthread_join(thread, NULL);
 	return (0);
 }
