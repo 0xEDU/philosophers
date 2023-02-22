7e49d632401689e963a1d900016100460382f4bc
etachott <etachott@student.42sp.org.br>
2023-02-22 17:22:35 -0300

    feat: Implemented forks fully (stable)

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,16 +6,26 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 23:02:09 by edu               #+#    #+#             */
-/*   Updated: 2023/02/21 23:55:02 by edu              ###   ########.fr       */
+/*   Updated: 2023/02/22 16:24:37 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "../includes/philo.h"
+#include <pthread.h>
+#include <unistd.h>
 
 void	eat(t_philo *philo, time_t sim_start)
 {
-	pthread_mutex_lock(philo->right_fork);
-	pthread_mutex_lock(philo->left_fork);
+	if (philo->id % 2)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		pthread_mutex_lock(philo->left_fork);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->left_fork);
+		pthread_mutex_lock(philo->right_fork);
+	}
 	print_state(philo, TAKE_FORK, sim_start);
 	print_state(philo, TAKE_FORK, sim_start);
 	print_state(philo, EAT, sim_start);
@@ -41,6 +51,8 @@ void	*simulation(void *ptr)
 	const time_t sim_start = get_current_time();
 	t_philo	*philo = ptr;
 
+	if (philo-> id % 2 == 0)
+		usleep(300 * 1000);
 	eat(philo, sim_start);
 	rest(philo, sim_start);
 	think(philo, sim_start);
