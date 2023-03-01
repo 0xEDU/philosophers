63f5ae04fb8dcda0522b6ff56f4648c8ae67200a
etachott <etachott@student.42sp.org.br>
2023-03-01 18:14:54 -0300

    refactor: moved unwanted libs

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,13 +6,11 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 23:02:09 by edu               #+#    #+#             */
-/*   Updated: 2023/03/01 15:56:32 by etachott         ###   ########.fr       */
+/*   Updated: 2023/03/01 18:13:39 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-#include <pthread.h>
-#include <unistd.h>
 
 int	main(int argc, char *argv[])
 {
